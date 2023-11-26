53032197f139da377d8807f12998d30896342d62
Henoker <hennybany@gmail.com>
2023-11-26 20:57:07 +0300

    feat: djoser and email config

--- backend/real_estate/urls.py
+++ backend/real_estate/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ] 
 
 admin.site.site_header = "Real Estate Admin"

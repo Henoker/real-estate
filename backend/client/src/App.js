0af8106ae63b44be39081699e92e2807a0dcf613
Henoker <hennybany@gmail.com>
2023-11-29 13:55:46 +0300

    chore: setup home and properties pages frontend

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import PropertiesPage from './pages/PropertiesPage';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> Welcome to Real Estate</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+          </Routes>
+          <Routes>
+            <Route path='/properties' element={<PropertiesPage/>}/>
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      <ToastContainer theme="dark" />
+    </>
   );
 }
 

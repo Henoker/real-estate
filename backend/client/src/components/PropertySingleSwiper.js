27bf82751896fe58b187638dbcc2f1aa7eb2c250
Henoker <hennybany@gmail.com>
2023-12-08 11:30:46 +0300

    feat: carousel fix on Property Swiper component

--- backend/client/src/components/PropertySingleSwiper.js
+++ backend/client/src/components/PropertySingleSwiper.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import { useSelector } from 'react-redux';
 
-const PropertySingleSwiper = () => {
-  // const { property } = useSelector((state) => state.properties);
 
-  // if (!property || !property.images || property.images.length === 0) {
-  //   return <div>No images available</div>;
-  // }
+const PropertySingleSwiper = ({property}) => {
+ 
 
   return (
     <div className="row justify-content-center">
       <div className="col-lg-8">
         <div id="property-single-carousel" className="swiper">
           <div className="swiper-wrapper">
-            {/* <Carousel data-bs-theme="dark">
-              {property.images.map((image, index) => (
-                <Carousel.Item key={index} className="carousel-item-b swiper-slide">
+            <Carousel data-bs-theme="dark">
+             
+                <Carousel.Item >
                   <img
-                    className="carousel-item-a swiper-slide d-block w-100"
-                    src={image} // Use the image from the backend
-                    alt={`Slide ${index + 1}`}
+                    className="d-block w-100 "
+                    src={"http://localhost:8000"+property.cover_photo} // Use the image from the backend
+                    alt= "slide1"
+                 
                   />
                 </Carousel.Item>
-              ))}
-            </Carousel> */}
+                <Carousel.Item >
+                  <img
+                    className="d-block w-100"
+                    src={"http://localhost:8000"+property.photo1} // Use the image from the backend
+                    alt= "slide2"
+                    
+                  />
+                </Carousel.Item>
+           
+            </Carousel>
           </div>
         </div>
         <div className="property-single-carousel-pagination carousel-pagination" />
